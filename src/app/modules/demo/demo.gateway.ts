ff96fc43f8d603a49a975a4d04117ad84ca13a4f
Ahmad Miqdaad <ahmadmiqdadz@gmail.com>
2024-10-16 20:35:04 +0800

    implement basic websocket

--- src/app/modules/demo/demo.gateway.ts
+++ src/app/modules/demo/demo.gateway.ts
@@ -1,25 +1,51 @@
-import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import * as dotenv from 'dotenv';
-import { Server } from 'socket.io';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { WebSocketGateway, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { Server, ServerOptions, WebSocket } from 'ws';
+import { HttpAdapterHost } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { IncomingMessage } from 'http';
+import * as url from 'url';
 
-// Load .env file
-dotenv.config();
+@Injectable()
+@WebSocketGateway()
+export class DemoGateway implements OnModuleInit, OnGatewayConnection, OnGatewayDisconnect {
+    private webSocketServer: Server;
 
-@WebSocketGateway({ namespace: process.env.SERVER_CONTEXT, cors: true })
-export class DemoGateway implements OnGatewayConnection, OnGatewayDisconnect {
-    @WebSocketServer() server: Server;
+    constructor(private readonly _httpAdapterHost: HttpAdapterHost) {}
 
-    handleConnection(client: any) {
-        console.log(`Client connected: ${client.id}`);
+    onModuleInit() {
+        const SERVER_ADDRESS = process.env.SERVER_ADDRESS || "127.0.0.1";
+        const SERVER_PORT = process.env.SERVER_PORT || 3000;
+        const SERVER_CONTEXT = process.env.SERVER_CONTEXT || "";
+        const httpServer = this._httpAdapterHost.httpAdapter.getHttpServer();
+
+        const options: ServerOptions = { server: httpServer, path: `/${SERVER_CONTEXT}` };
+        // Attach WebSocket server to the existing NestJS HTTP server
+        this.webSocketServer = new Server(options);
+
+        this.webSocketServer.on('connection', (webSocket: WebSocket, request: IncomingMessage) => {
+
+            const queryParams = url.parse(request.url || '', true).query;
+            Logger.debug(`Client connected `, "BasicGateway");
+            
+            webSocket.on('message', (message) => {
+                console.log('Received:', message);
+                webSocket.send(`Echo: ${message}`); // Echo back the received message
+            });
+
+            webSocket.on('close', () => {
+                Logger.debug(`Client disconnected`, "BasicGateway");
+            });
+        });
+
+        Logger.log(`NestJS app is running on http://${SERVER_ADDRESS}:${SERVER_PORT}/${SERVER_CONTEXT}`, "BasicGateway");
     }
 
-    handleDisconnect(client: any) {
-        console.log(`Client disconnected: ${client.id}`);
+    handleConnection() {
+        // Logic when a new client connects
     }
 
-    @SubscribeMessage('message')
-    handleMessage(client: any, payload: any) {
-        console.log(`message: ${payload}`)
-        this.server.emit('message', payload);
+    handleDisconnect() {
+        // Logic when a client disconnects
     }
 }
